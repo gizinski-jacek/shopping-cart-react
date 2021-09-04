8d1955ddb2359d313da56aac5258c0b56eee7bd5
mrjackmaster <jacektrg+github@gmail.com>
2021-09-04 22:43:27 +0200

    Long overdue commit. Routing properly components.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
+import { useState } from 'react';
 
 function App() {
+	const [showCart, setShowCart] = useState(false);
+
+	const toggleCartDisplay = () => {
+		setShowCart((prevState) => !prevState);
+	};
+
 	return (
 		<BrowserRouter>
+			<NavBar toggle={toggleCartDisplay} />
 			<Switch>
 				<Route exact path='/' component={Home} />
 				<Route exact path='/shop' component={Shop} />
-				<Route exact path='/cart' component={Cart} />
 			</Switch>
+			{showCart ? <Cart toggle={toggleCartDisplay} /> : null}
 		</BrowserRouter>
 	);
 }
