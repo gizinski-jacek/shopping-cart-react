83ba5374624e58b2d9ff95254860c686f07df835
mrjackmaster <jacektrg+github@gmail.com>
2021-09-04 22:51:23 +0200

    Component for rendering more detailed info about specific type of item, still WIP.

---
+++ src/components/utils/DetailsMboard.js
@@ -0,0 +1,26 @@
+function DetailsMboard(props) {
+	const {
+		categoryId,
+		name,
+		price,
+		cores,
+		threads,
+		tdp,
+		coreClock,
+		boostClock,
+		gallery,
+	} = props.data;
+	const { toggle } = props;
+
+	const galleryDisplay = gallery.map((item) => {
+		return <img src={item} alt='' />;
+	});
+
+	return (
+		<div className='detailsCard'>
+			<div className='gallery'>{galleryDisplay}</div>
+		</div>
+	);
+}
+
+export default DetailsMboard;
