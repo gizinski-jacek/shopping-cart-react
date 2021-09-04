210e16e88157751f7b77215358510208c7cde8a0
mrjackmaster <jacektrg+github@gmail.com>
2021-09-04 22:46:11 +0200

    Long overdue commit. Really nothing here yet, future setup for cart.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,13 @@
-function Cart() {
-	return <div>Cart</div>;
+function Cart(props) {
+	return (
+		<div className='cart'>
+			<div className='divRelative'>
+				<span className='close' onClick={props.toggle}>
+					<img src='/assets/close.svg' alt='' />
+				</span>
+			</div>
+		</div>
+	);
 }
 
 export default Cart;
