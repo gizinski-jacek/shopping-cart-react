2dabf190f863e5a61c1c98dacfe1afb2a3abd288
mrjackmaster <jacektrg+github@gmail.com>
2021-09-05 19:12:04 +0200

    Buttons handlers and state for quantity of items.

--- src/components/utils/ProductCard.js
+++ src/components/utils/ProductCard.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 function ProductCard(props) {
 	const { categoryId, name, price, preview } = props.data;
 	const [showModal, setShowModal] = useState('none');
+	const [itemQuantity, setItemQuantity] = useState(1);
 
 	const toggleCardOn = () => {
 		setShowModal('flex');
@@ -14,23 +15,48 @@ function ProductCard(props) {
 		}
 	};
 
+	const decrement = () => {
+		if (itemQuantity > 1) {
+			setItemQuantity((prevState) => prevState - 1);
+		}
+	};
+
+	const increment = () => {
+		if (itemQuantity < 100) {
+			setItemQuantity((prevState) => prevState + 1);
+		}
+	};
+
 	return (
 		<div className='productCard'>
 			<img className='productPreview' src={preview} alt={name} />
 			<div className='productInfo'>
 				<div className='productName'>{name}</div>
-				<div className='productPrice'>{price}</div>
-				<p className='productDetails' onClick={toggleCardOn}>
-					More Info
-				</p>
-				<div
-					className='detailsModal'
-					style={{ display: showModal }}
-					onClick={(e) => {
-						toggleCardOff(e);
-					}}
-				>
-					{props.children}
+				<div>
+					<div>
+						<div className='productDetails' onClick={toggleCardOn}>
+							Details
+						</div>
+						<div className='productPrice'>Price: {price}</div>
+					</div>
+					<div className='quantityControls'>
+						<button className='decrementBtn' onClick={decrement}>
+							&#8722;
+						</button>
+						<input className='itemQuantity' value={itemQuantity} />
+						<button className='incrementBtn' onClick={increment}>
+							&#43;
+						</button>
+					</div>
+					<div
+						className='detailsModal'
+						style={{ display: showModal }}
+						onClick={(e) => {
+							toggleCardOff(e);
+						}}
+					>
+						{props.children}
+					</div>
 				</div>
 			</div>
 		</div>
