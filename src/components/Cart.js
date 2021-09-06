8301fd1909ce62bd26123f4f274914156123197c
mrjackmaster <jacektrg+github@gmail.com>
2021-09-06 23:06:14 +0200

    Rendering cart contents with wrapper component.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,31 @@
-function Cart(props) {
+import CartItemWrapper from './utils/CartItemWrapper';
+
+const Cart = (props) => {
+	const { data, toggle } = props;
+
+	const content = data.map((item, index) => {
+		return (
+			<CartItemWrapper
+				key={item.categoryId + index + item.Name}
+				data={item}
+				remove={props.remove}
+			/>
+		);
+	});
+
 	return (
 		<div className='cart'>
-			<div className='divRelative'>
-				<span className='close' onClick={props.toggle}>
-					<img src='/assets/close.svg' alt='' />
-				</span>
+			<div className='cartControls'>
+				<button className='checkout' onClick={toggle}>
+					Checkout
+				</button>
+				<button className='close' onClick={toggle}>
+					Close
+				</button>
 			</div>
+			<div className='cartContents'>{content}</div>
 		</div>
 	);
-}
+};
 
 export default Cart;
