506004904944e7facb93fa4ecca9396134bbfcbb
mrjackmaster <jacektrg+github@gmail.com>
2021-09-07 17:47:02 +0200

    Updating total cart value whenever contents of cart change. Refactored function adding items to cart to update item count if same one is already in cart.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const App = () => {
 	const [showCart, setShowCart] = useState(false);
 	const [cartContents, setCartContents] = useState([]);
-	console.log(cartContents);
+	const [totalItemPrice, setTotalItemPrice] = useState(0);
+	const [totalItemCount, setTotalItemCount] = useState(0);
 
 	const toggleCartDisplay = () => {
 		setShowCart((prevState) => !prevState);
 	};
 
-	const addItemToCart = (item) => {
-		setCartContents((prevState) => [...prevState, item]);
+	const addItemToCart = (item, quantity) => {
+		const duplicate = cartContents.find((dup) => dup === item);
+		if (duplicate) {
+			setCartContents((prevState) => [
+				...prevState.filter((item) => item !== duplicate),
+			]);
+			duplicate.count += quantity;
+			setCartContents((prevState) => [...prevState, duplicate]);
+		} else {
+			item.count = quantity;
+			setCartContents((prevState) => [...prevState, item]);
+		}
 	};
 
 	const removeItemFromCart = (remove) => {
@@ -25,14 +36,31 @@ const App = () => {
 		]);
 	};
 
+	useEffect(() => {
+		setTotalItemPrice(
+			cartContents
+				.map((item) => item.Price * item.count)
+				.reduce((a, b) => a + b, 0)
+				.toFixed(2)
+		);
+		setTotalItemCount(
+			cartContents.map((item) => item.count).reduce((a, b) => a + b, 0)
+		);
+	}, [cartContents]);
+
 	return (
 		<BrowserRouter>
-			<NavBar toggle={toggleCartDisplay} />
+			<NavBar
+				toggle={toggleCartDisplay}
+				totalPrice={totalItemPrice}
+				totalCount={totalItemCount}
+			/>
 			<Switch>
 				<Route exact path='/' component={Home} />
 				<Route exact path='/shop'>
 					<Shop addToCart={addItemToCart} />
 				</Route>
+				<Route exact path='/checkout' component={Home} />
 			</Switch>
 			{showCart ? (
 				<Cart
