9cde8abe6a4546c214c46ef252895155e50b27f5
mrjackmaster <jacektrg+github@gmail.com>
2021-09-07 17:45:08 +0200

    Cleanup, linking to checkout page.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
 import CartItemWrapper from './utils/CartItemWrapper';
 
 const Cart = (props) => {
 	const { data, toggle } = props;
+	const { remove } = props;
 
 	const content = data.map((item, index) => {
 		return (
 			<CartItemWrapper
 				key={item.categoryId + index + item.Name}
 				data={item}
-				remove={props.remove}
+				remove={remove}
 			/>
 		);
 	});
@@ -16,11 +18,11 @@ const Cart = (props) => {
 	return (
 		<div className='cart'>
 			<div className='cartControls'>
-				<button className='checkout' onClick={toggle}>
-					Checkout
-				</button>
+				<Link to='/checkout' className='checkout' onClick={toggle}>
+					CHECKOUT
+				</Link>
 				<button className='close' onClick={toggle}>
-					Close
+					CLOSE
 				</button>
 			</div>
 			<div className='cartContents'>{content}</div>
