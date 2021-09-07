e9484bb377f55ac6afceaff3cd848d93ac2814b6
mrjackmaster <jacektrg+github@gmail.com>
2021-09-07 17:41:53 +0200

    Updating

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -8,26 +8,27 @@ import {
 import CatalogPage from './CatalogPage';
 
 const Shop = (props) => {
+	const { addToCart } = props;
 	let match = useRouteMatch();
 	return (
 		<BrowserRouter>
 			<div className='content'>
 				<div className='productMenu'>
 					<Link to={match.url}>All Products</Link>
-					<Link to={match.url + '/cpu'}>Processors</Link>
+					<Link to={match.url + '/cpu'}>Processor</Link>
 					<Link to={match.url + '/gpu'}>Video Card</Link>
 					<Link to={match.url + '/memory'}>Memory</Link>
-					<Link to={match.url + '/mboard'}>Motherboard</Link>
+					<Link to={match.url + '/motherboard'}>Motherboard</Link>
 					<Link to={match.url + '/psu'}>Power Supply</Link>
 					<Link to={match.url + '/storage'}>Storage</Link>
 					<Link to={match.url + '/chassis'}>Chassis</Link>
 				</div>
 				<Switch>
 					<Route exact path={match.path}>
-						<CatalogPage addToCart={props.addToCart} />
+						<CatalogPage addToCart={addToCart} />
 					</Route>
 					<Route exact path={match.path + '/:productType'}>
-						<CatalogPage addToCart={props.addToCart} />
+						<CatalogPage addToCart={addToCart} />
 					</Route>
 				</Switch>
 			</div>
