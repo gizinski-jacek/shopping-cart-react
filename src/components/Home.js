82a1f1a8ddc8c9ce94b4feaf99e952fbdc99e27a
mrjackmaster <jacektrg+github@gmail.com>
2022-04-23 18:43:40 +0200

    Minor addition to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-	return <div className='home'>Home</div>;
+	return (
+		<div className='home'>
+			<h2>Build your dream PC with the best parts</h2>
+			<Link to='/shop'>SHOP NOW</Link>
+		</div>
+	);
 };
 
 export default Home;
